794348a418eac885571dbdab14bdcc782adafb66
Miguel Paredes <miguelparedesaceves@gmail.com>
2025-02-04 20:41:57 -0600

    Ejercicios 3 y 4 de práctica 04

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -10,6 +10,8 @@
 número de letras, números y guion bajo.</p>
 
 <?php
+error_reporting(0);
+
 $a = "ManejadorSQL";
 $b = 'MySQL';
 $c = &$a;
@@ -25,7 +27,43 @@ echo "$a, ";
 echo "$b, ";
 echo "$c <br>";
 
+echo "<p>En el segundo bloque de asignaciones la variable \$a se le asigna PHP server y a la variable \$b se le asigna el contenido de la variable a  <br>";
+unset($a);
+unset($b);
+unset($c);
 
+
+echo "<br><br> 3):<br>";
+
+$a = "PHP5";
+var_dump($a);
+echo "<br>";
+$z[] = &$a;
+var_dump($z);
+echo "<br>";
+$b = "5a version de PHP";
+var_dump($b);
+echo "<br>";
+@$c = $b*10;
+var_dump($c);
+echo "<br>";
+$a .= $b;
+var_dump($a);
+echo "<br>";
+@$b *= $c;
+var_dump($b);
+echo "<br>";
+$z[0] = "MySQL";
+var_dump($z);
+echo "<br>";
+
+
+echo "<br><br> 4):<br>";
+echo "GLOBALS['a']: " . $GLOBALS['a'] . "<br>";
+echo "GLOBALS['b']: " . $GLOBALS['b'] . "<br>";
+echo "GLOBALS['c']: " . $GLOBALS['c'] . "<br>";
+echo "GLOBALS['z']: ";
+print_r($GLOBALS['z']);
 ?>
 
 </body>
